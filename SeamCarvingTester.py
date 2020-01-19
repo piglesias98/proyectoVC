7b119f8d3196b92e37a11f2629d8c9bb5bd99af8
piglesias98 <33418308+piglesias98@users.noreply.github.com>
2020-01-19 15:47:44 +0100

    Añadida función scaleAndRemove

--- SeamCarvingTester.py
+++ SeamCarvingTester.py
@@ -10,6 +10,7 @@ import sys
 import numpy as np
 
 from matplotlib import pyplot as plt
+#from skimage.feature import hog
 
 
 import SeamCarving
@@ -54,18 +55,64 @@ def representar_imagenes(lista_imagen_leida, lista_titulos, n_col=2, tam=15):
 
 
 # Prueba de funcionamiento
-image = SeamCarving.readImage("surfista-min.jpeg", 1)
 
-img = image.copy()
 
-simple_energy = SeamCarving.simpleEnergy(img)
-simple_energy2 = SeamCarving.simpleEnergyRGB(img)
+#simple_energy = SeamCarving.simpleEnergy(img)
+#simple_energy2 = SeamCarving.eHOG(img)
 
 #img2 = cv2.cvtColor(simple_energy, cv2.COLOR_BGR2RGB)
 #simple_energy2 = cv2.cvtColor(simple_energy2, cv2.COLOR_BGR2RGB)
 
-representar_imagenes([simple_energy, simple_energy2], ["energia1", "energia1"])
 
+#for i in range(500):
+#        for j in range(500):
+#            if i-100<0: inicio_i=0
+#            else: inicio_i = i-100
+#            if i+101>image.shape[0]: fin_i = image.shape[0]
+#            else: fin_i = i+101
+#            
+#            if j-100<0: inicio_j=0
+#            else: inicio_j = j-5
+#            if j+101>image.shape[0]: fin_j = image.shape[1]
+#            else: fin_j = j+101
+#            
+#            window = image[inicio_i:fin_i,inicio_j:fin_j]
+#            imagenes.append(window)
+#            titulos.append("windo2")
+
+
+
+
+#window = img[0:100, 0:100]
+#hog = cv2.HOGDescriptor()
+#h = hog.compute(img)
+#print(h)
+#hogg, img2 = hog(img, orientations = 8, visualize=True)
+#print(hogg)
+#representar_imagenes([img2],["titulos"])
+
+
+#matriz = np.matrix('1, 2, 3, 4; 3, 5, 6, 7; 8, 9, 10, 11; 12, 13, 14, 15' )
+#print(matriz)
+#print(matriz.shape[0])
+#maximos=np.zeros(matriz.shape)
+#for i in range(matriz.shape[0]):
+#    for j in range(matriz.shape[1]):
+#        print("elemento", matriz[i,j])
+#        if i-1<0: inicio_i=0
+#        else: inicio_i = i-1
+#        if i+2>matriz.shape[0]: fin_i = matriz.shape[0]
+#        else: fin_i = i+2
+#        
+#        if j-1<0: inicio_j=0
+#        else: inicio_j = j-1
+#        if j+2>matriz.shape[0]: fin_j = matriz.shape[1]
+#        else: fin_j = j+2
+#        
+#        a = matriz[inicio_i:fin_i,inicio_j:fin_j]
+#        maximos[i,j] = np.max(a)
+#        print("ventana", a)
+#print("maximos",maximos)
 
 #cv2.imshow("original", image)
 #cv2.imshow("energia 1", simple_energy)
@@ -73,10 +120,70 @@ representar_imagenes([simple_energy, simple_energy2], ["energia1", "energia1"])
 #
 #cv2.waitKey(0)
 #cv2.destroyAllWindows()
+img = SeamCarving.readImage("playa.jpg", 1)
+nn = 200
+nm = 400
+
+imagenes=[]
+n, m = img.shape[:2]
+print("n", n)
+print("m", m)
+print("nn",nn)
+print("nm", nm)
+print("nn/n", nn/n)
+print("nm/m", nm/m)
+scale_factor = max(nn/n, nm/m)
+print(scale_factor)
+height = int(n * scale_factor)
+print("hegiht", height)
+width = int (m * scale_factor)
+print("widht", width)
+dim = (width, height)
+# resize image
+resized = cv2.resize(img, dim)
+print("resized schape", resized.shape)
+#Eliminamos las verticales o horizontales que sobren
+print("sobran ", height - nn, " horizontales")
+print("sobran ", width - nm, " verticales")
+for i in range(height - nn):
+    rotada = np.rot90(resized, 1)
+    a, b, path = SeamCarving.verticalSeam(rotada)
+    rotada = SeamCarving.removeSeam (rotada, path, 1)
+    resized = np.rot90(rotada, 1)
+for i in range(width - nm):
+    a, b, path = SeamCarving.verticalSeam(resized)
+    resized = SeamCarving.removeSeam(resized, path, 1)
+print("shape final",resized.shape)
+imagenes.append(resized)
+
+img4 = SeamCarving.readImage("playa.jpg", 1)
 
 
-#T, options = seamsOrder(image, image.shape[0], image.shape[1]-100)
-#order = selectSeamsOrder (image, T, options)
+T, options = SeamCarving.seamsOrder(img4, 200, 400)
+order = SeamCarving.selectSeamsOrder (img4, T, options)
+seams = SeamCarving.removeOrderSeams (img4, order)
 
-#img1 = addOrderSeams (image, order)
-#img2 = removeOrderSeams (image, order)
+imagenes.append(seams)
+representar_imagenes(imagenes,["resizes", "seams"],1)
+#scale_factor = max(nn/n, nm/m)
+#print("nn/n", nn/n, "nm/m", nm/m)
+#print("factor", scale_factor)
+#height = int(n * scale_factor)
+#print("height", height)
+#width = int (m * scale_factor)
+#print("width", width)
+#dim = (width, height)
+## resize image
+#resized = cv2.resize(image, dim)
+#print("resized shape", resized.shape)
+#representar_imagenes([image, resized], ["image", "resized"])
+
+#T, options = SeamCarving.seamsOrder(img, 200, 400)
+#order = SeamCarving.selectSeamsOrder (image, T, options)
+
+#img1 = SeamCarving.addOrderSeams (image, order)
+#img2 = SeamCarving.scaleAndRemoveSeams(image, 200, 400)
+#img1 = SeamCarving.removeOrderSeams (img, order)
+#
+#
+#representar_imagenes([img1, img2],["sin scale", "con scale"],1)
