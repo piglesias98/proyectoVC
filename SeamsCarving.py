4630410263d38ea41e8bdac33e053e78e861234a
piglesias98 <33418308+piglesias98@users.noreply.github.com>
2020-01-20 17:35:28 +0100

    objectRemoval pero falta implementar contentAwareResizing para que funcione

--- SeamsCarving.py
+++ SeamsCarving.py
@@ -5,3 +5,19 @@ import numpy as np
 
 import Basics
 import energias
+
+
+
+def objectRemoval(image, remove_mask=None, preserve_mask=None, nn=0, nm=0):
+    
+    energy = energias.forwardEnergy(image)
+    
+    if remove_mask != None:
+        nn, nm = Basics.maskSize(remove_mask)
+        energy = Basics.removeEnergy(energy, remove_mask)
+        
+    if preserve_mask != None:
+        energy = Basics.removeEnergy(energy, preserve_mask)
+    
+    return contentAwareResizing(image, nn, nm, True,energy)
+        
